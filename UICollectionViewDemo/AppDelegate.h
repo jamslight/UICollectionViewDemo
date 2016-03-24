9a2df5f1989a58fed482f2729719f19f7eb9002c
jamslight <define@appnorihantekimacbook-air.local>
2016-03-24 21:56:04 +0800

    Initial Commit

---
+++ UICollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewDemo
+//
+//  Created by define on 16/3/24.
+//  Copyright © 2016年 LiuShaoQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
